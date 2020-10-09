7d7cba30520bf63485b5d40c6828975c6f60b092
Jelle Tiepel <414532@student.fontys.nl>
2020-10-09 14:17:57 +0200

    Ophalen boektitel
    Ik heb geprobeerd de boektitels op te halen naar de console maar dit is niet gelukt.

--- Appshell.js
+++ Appshell.js
@@ -3,4 +3,9 @@
 let url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=72qx6ixSuFxjcKBGV3RIBSA6QAjC96WX"
 fetch(url)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data);
+    data.books.map(book => {
+      console.log(book.title);
+    })
+  })
