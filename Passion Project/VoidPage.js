eb687cae1e9de0cd09e650508f0beac121aea549
Jelle Tiepel <414532@student.fontys.nl>
2021-01-11 15:47:50 +0100

    Individuele paginas met animaties 2
    Het grootste deel van de individuele 3 pagina's zijn nu klaar, bij de creaturespagina moeten alleen nog de afbeeldingen correct geschaald worden, maar object-fit: cover werkt niet mee.

--- Passion Project/VoidPage.js
+++ Passion Project/VoidPage.js
@@ -9,7 +9,19 @@ $(document).ready(function(){
     $(".display").fadeOut(500);
   });
 });
+
+
 //Geeft een delay voordat de selectiepagina verdwijnt
 function delay (URL) {
     setTimeout( function() { window.location = URL }, 1200 );
+}
+
+
+var imageArray = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12", "image13", "image14", "image15", "image16", "image17", "image18", "image19", "image20", "image21", "image22", "image23", "image24", "image25", "image26", "image27", "image28", "image29", "image30", ]
+
+
+var sc = document.querySelector("#creatureSelection")
+for(var i=0; i < imageArray.length; i++){
+   var img = sc.appendChild(document.createElement("img"));
+   img.src = "creatureImages/image" + (i + 1) + ".jpg";
 }
