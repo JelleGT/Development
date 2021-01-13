06e2688d32dd3133fe6c9df8a3f03c30a6122ff3
Jelle Tiepel <414532@student.fontys.nl>
2021-01-13 13:10:42 +0100

    Creaturepagina 1
    Er zijn modals toegevoegd bij de afbeeldingen, er moet nog gekeken worden naar de juiste verhouding en werking

--- Passion Project/VoidPage.js
+++ Passion Project/VoidPage.js
@@ -19,12 +19,27 @@ function delay (URL) {
     setTimeout( function() { window.location = URL }, 1200 );
 }
 
+//De sectie van de pagina waar alle creatures in komen te staan
+var sc = document.querySelector("#creatureSelection");
 
-var imageArray = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12", "image13", "image14", "image15", "image16", "image17", "image18", "image19", "image20", "image21", "image22", "image23", "image24", "image25", "image26", "image27", "image28", "image29", "image30", ]
-
-
-var sc = document.querySelector("#creatureSelection")
-for(var i=0; i < imageArray.length; i++){
-   var img = sc.appendChild(document.createElement("img"));
-   img.src = "creatureImages/image" + (i + 1) + ".jpg";
+var a = sc.getElementsByTagName('a');
+var img = sc.getElementsByTagName('img');
+//Er worden a en img elementen aangemaakt
+for(var i=0; i < 30; i++){
+   var link = sc.appendChild(document.createElement("a"));
+   var modal = link.appendChild(document.createElement("img"));
+   link.href = "creatureImages/image" + (i + 1) + ".jpg";
+   modal.src = "creatureImages/image" + (i + 1) + ".jpg"
 }
+
+//Dit zet de modals klaar
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
+
+//Per creature komt er een modal met dezelfde afbeelding
+$('#creatureSelection').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image'
+  // other options
+});
